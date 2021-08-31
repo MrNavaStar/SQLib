ccf7906fb3e761c5883d4f31f59f7041016cdaec
Ethan <Mr.NavaStar@gmail.com>
2021-08-30 23:36:03 -0600

    keep forgetting to remove test code

--- src/main/java/mrnavastar/sqlib/Sqlib.java
+++ src/main/java/mrnavastar/sqlib/Sqlib.java
@@ -10,22 +10,6 @@ import net.minecraft.nbt.NbtCompound;
 public class Sqlib implements ModInitializer {
     @Override
     public void onInitialize() {
-        Database.TYPE = "SQLITE";
-        Database.DATABASE_NAME = "MyAmazingDatabase";
-        Database.SQLITE_DIRECTORY = "/home/ethan/test";
 
-        Database.init();
-
-        Table asd = new Table("sdad");
-        DataContainer test = new DataContainer("sdad");
-        asd.put(test);
-
-        test.put("S", "sdad");
-        test.put("S", 1);
-        test.put("S", true);
-        test.put("S", new JsonObject());
-        NbtCompound s = new NbtCompound();
-        s.putInt("sda", 1);
-        test.put("S", s);
     }
 }
