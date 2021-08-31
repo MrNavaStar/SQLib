a349e3181151a06d50e658a4105029ff81f8e5df
Ethan <Mr.NavaStar@gmail.com>
2021-08-30 22:33:13 -0600

    forgot to remove test code

--- src/main/java/mrnavastar/sqlib/Sqlib.java
+++ src/main/java/mrnavastar/sqlib/Sqlib.java
@@ -1,23 +1,10 @@
 package mrnavastar.sqlib;
 
-import mrnavastar.sqlib.api.DataContainer;
-import mrnavastar.sqlib.api.Table;
-import mrnavastar.sqlib.util.Database;
 import net.fabricmc.api.ModInitializer;
 
 public class Sqlib implements ModInitializer {
     @Override
     public void onInitialize() {
-        Database.TYPE = "SQLITE";
-        Database.DATABASE_NAME = "POGGERS";
-        Database.SQLITE_DIRECTORY = "/home/ethan/test";
 
-        Database.init();
-
-        Table table = new Table("Ssdsad");
-        DataContainer data = new DataContainer("123");
-        table.put(data);
-        
-        System.out.println(table.get("123").getId());
     }
 }
