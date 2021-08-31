97bbcb2ae61fd9352c4f1be6d9e747bdcea1eadd
Ethan <Mr.NavaStar@gmail.com>
2021-08-30 19:29:28 -0600

    no more enums

--- src/main/java/mrnavastar/sqlib/api/SqlTypes.java
+++
@@ -1,7 +0,0 @@
-package mrnavastar.sqlib.api;
-
-public enum SqlTypes {
-
-    SQLITE,
-    MYSQL
-}
--- src/main/java/mrnavastar/sqlib/util/Database.java
+++ src/main/java/mrnavastar/sqlib/util/Database.java
@@ -1,13 +1,12 @@
 package mrnavastar.sqlib.util;
 
-import mrnavastar.sqlib.api.SqlTypes;
 import mrnavastar.sqlib.api.Table;
 
 import java.util.ArrayList;
 
 public class Database {
 
-    public static Enum<SqlTypes> TYPE;
+    public static String TYPE;
     //General
     public static String DATABASE_NAME;
     //SQLITE
@@ -20,22 +19,9 @@ public class Database {
 
     private static final ArrayList<Table> tables = new ArrayList<>();
 
-
-    public static void setType(Enum<SqlTypes> type) {
-        TYPE = type;
-    }
-
-    public static void setDatabaseName(String name) {
-        DATABASE_NAME = name;
-    }
-
-    public static void setSqliteDirectory(String path) {
-        SQLITE_DIRECTORY = path;
-    }
-
     public static void connect() {
-        if (TYPE.equals(SqlTypes.SQLITE)) SqlManager.connectSQLITE(SQLITE_DIRECTORY, DATABASE_NAME);
-        if (TYPE.equals(SqlTypes.MYSQL)) SqlManager.connectMYSQL(MYSQL_ADDRESS, MYSQL_PORT, DATABASE_NAME, MYSQL_USERNAME, MYSQL_PASSWORD);
+        if (TYPE.equals("SQLITE")) SqlManager.connectSQLITE(SQLITE_DIRECTORY, DATABASE_NAME);
+        if (TYPE.equals("MYSQL")) SqlManager.connectMYSQL(MYSQL_ADDRESS, MYSQL_PORT, DATABASE_NAME, MYSQL_USERNAME, MYSQL_PASSWORD);
     }
 
     public static void disconnect() {
