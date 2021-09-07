ff4f2a9ca4175a06d53b8b28997630e45a315eb5
Ethan <Mr.NavaStar@gmail.com>
2021-09-06 18:32:28 -0600

    Fixed UUIDS

--- src/main/java/mrnavastar/sqlib/util/SqlManager.java
+++ src/main/java/mrnavastar/sqlib/util/SqlManager.java
@@ -61,7 +61,7 @@ public class SqlManager {
 
     public static void createTable(String name) {
         try {
-            String sql = "CREATE TABLE IF NOT EXISTS " + name + " (ID TEXT PRIMARY KEY, STRINGS TEXT, INTS TEXT, FLOATS TEXT, DOUBLES TEXT, BOOLEANS TEXT, JSON TEXT, NBT TEXT, BLOCKPOS TEXT)";
+            String sql = "CREATE TABLE IF NOT EXISTS " + name + " (ID TEXT PRIMARY KEY, STRINGS TEXT, INTS TEXT, FLOATS TEXT, DOUBLES TEXT, BOOLEANS TEXT, JSON TEXT, NBT TEXT, BLOCKPOS TEXT, UUIDS TEXT)";
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setQueryTimeout(30);
             stmt.executeUpdate();
