d4deb08c3947992cd426e6c6e8c25fa1545a71e3
MrNavaStar <Mr.NavaStar@gmail.com>
2023-10-08 21:43:45 -0600

    it works! Remove testing code

--- src/main/java/mrnavastar/sqlib/SQLib.java
+++ src/main/java/mrnavastar/sqlib/SQLib.java
@@ -11,27 +11,12 @@ import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 
-public class SQLib implements ModInitializer {
+public class SQLib {
 
     public static final String MOD_ID = "SQLib";
     public static final Gson GSON = new Gson();
 
     public static void log(Level level, String message) {
         LogManager.getLogger().log(level, "[" + MOD_ID + "] " + message);
-    }
-
-    @Override
-    public void onInitialize() {
-        Database d = new SQLiteDatabase(MOD_ID, "test", "config");
-        Table t = d.createTable("poggers").addColumn("t", SQLDataType.MUTABLE_TEXT).finish();
-
-        MutableText pain = (MutableText) Text.of("pof");
-        pain.formatted(Formatting.RED);
-
-        DataContainer testy = t.createDataContainer("poggggg");
-        testy.put("t", pain);
-
-        MutableText ppp = testy.getMutableText("t");
-        System.out.println(ppp.toString());
     }
 }
