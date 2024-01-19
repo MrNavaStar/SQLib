837d76c5094fd78f6fe34c8c8404fd29ac0556ce
MrNavaStar <Mr.NavaStar@gmail.com>
2024-01-18 20:32:50 -0700

    fix crash when using on client (based)

--- src/main/java/me/mrnavastar/sqlib/Fabric.java
+++ src/main/java/me/mrnavastar/sqlib/Fabric.java
@@ -1,15 +1,27 @@
 package me.mrnavastar.sqlib;
 
 import me.mrnavastar.sqlib.database.Database;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
-public class Fabric extends SQLib implements PreLaunchEntrypoint {
+public class Fabric extends SQLib implements PreLaunchEntrypoint, DedicatedServerModInitializer, ClientModInitializer {
 
     @Override
     public void onPreLaunch() {
-        ServerLifecycleEvents.SERVER_STOPPING.register(server -> databases.forEach(Database::close));
         init(FabricLoader.getInstance().getGameDir(), FabricLoader.getInstance().getConfigDir());
+    }
+
+    @Override
+    public void onInitializeServer() {
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> databases.forEach(Database::close));
+    }
+
+    @Override
+    public void onInitializeClient() {
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> databases.forEach(Database::close));
     }
 }
