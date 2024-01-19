b55354e80566eb2185bbe87b7ef3563788881168
MrNavaStar <Mr.NavaStar@gmail.com>
2024-01-19 02:07:58 -0700

    make it run on any bukkit version

--- src/main/java/me/mrnavastar/sqlib/Paper.java
+++ src/main/java/me/mrnavastar/sqlib/Paper.java
@@ -3,8 +3,6 @@ package me.mrnavastar.sqlib;
 import me.mrnavastar.sqlib.database.Database;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.nio.file.Path;
-
 public class Paper extends JavaPlugin {
 
     @Override
